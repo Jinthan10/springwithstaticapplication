c439aaa1d5ff2f2e0f7dcaa6d5acf557f4ab6d1e
Jinthan10 <153614490+Jinthan10@users.noreply.github.com>
2025-05-16 12:53:18 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cd /home/ec2-user/app
+echo "Starting Spring Boot application..."
+
+cd /home/ec2-user/app || exit 1
+
+# Run app in background
 nohup java -jar spring.jar > app.log 2>&1 &
+
+echo "App started successfully."
